01b051aa66c00e2f26c845a882e66c1befce787e
Volker <shenyun_gu@hotmail.com>
2024-05-20 12:48:28 +0800

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by 顾沈云 on 2024/5/20.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
